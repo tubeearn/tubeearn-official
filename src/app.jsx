77d13d03716e41506b03282b2b6376003bca8fd9
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-08-04 12:02:12 +0530

    Delete src directory

--- src/app.jsx
+++
@@ -1,6 +0,0 @@
-// App.jsx import React from "react"; import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; import Sidebar from "./components/Sidebar"; import Topbar from "./components/Topbar"; import Dashboard from "./pages/Dashboard"; import Wallet from "./pages/Wallet"; import VideoUpload from "./pages/VideoUpload"; import Settings from "./pages/Settings";
-
-function App() { return ( <Router> <div className="flex h-screen"> <Sidebar /> <div className="flex-1 flex flex-col"> <Topbar /> <main className="p-4 overflow-y-auto flex-1"> <Routes> <Route path="/" element={<Dashboard />} /> <Route path="/wallet" element={<Wallet />} /> <Route path="/upload" element={<VideoUpload />} /> <Route path="/settings" element={<Settings />} /> </Routes> </main> </div> </div> </Router> ); }
-
-export default App;
-
